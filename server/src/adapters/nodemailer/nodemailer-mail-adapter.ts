3ade4e55228e69dbb120216eee716c0e69ea9317
larimoreira21 <lari2102@gmail.com>
2022-05-16 17:10:15 -0300

    primeiro commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "cb0d1fa82b06e5",
+    pass: "97912d03074ca6",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Larissa Moreira <lari2102@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
